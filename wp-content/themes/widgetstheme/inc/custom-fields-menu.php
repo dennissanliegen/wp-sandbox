59f8d719ebc616e7455054c5220e54a6a8f705a1
dz <denniszyche@Denniss-MBP.fritz.box>
2017-08-28 12:03:44 +0200

    Bug Fixed (->Add Custom Field)

--- wp-content/themes/widgetstheme/inc/custom-fields-menu.php
+++ wp-content/themes/widgetstheme/inc/custom-fields-menu.php
@@ -1,86 +1,57 @@
-<?php
+<?php 
+// Create fields
+// Show columns
+// Save/Update fields
+// Update the Walker nav
+function fields_list() {
 
-/*
-
-@package wpsandbox
-
-  =====================================
-    Create fields
-    Show columns
-    Save/Update fields
-    Update the Walker nav
-  =====================================
-
-*/
-
-// Creat Fields
-function fields_list(){
 	return array(
-		'megamenu' => 'Activate MegaMenu',
-		'column-divider' => 'Column Divider',
-		'featured-image' => 'Feautured Image',
+		'megamenu' => 'Activate Menu',
+		'column-divider' => 'Divider',
 	);
 }
-
-// Setup Fields
-function megamenu_fields( $id, $item, $depth, $args ){
-	
+// Setup fields
+function megamenu_fields( $id, $item, $depth, $args ) {
 	$fields = fields_list();
-
 	foreach ( $fields as $_key => $label ) :
-		
-		$key = sprintf( 'menu-item-%s', $_key );
-		$id = sprintf( 'edit-%s-%s', $key, $item->ID );
-		$name = sprintf( '%s[%s]', $key, $item->ID );
+		$key   = sprintf( 'menu-item-%s', $_key );
+		$id    = sprintf( 'edit-%s-%s', $key, $item->ID );
+		$name  = sprintf( '%s[%s]', $key, $item->ID );
 		$value = get_post_meta( $item->ID, $key, true );
 		$class = sprintf( 'field-%s', $_key );
-
 		?>
-			<p class="description description-wide <?php esc_attr( $class ) ?>">
-				<label for="<?php esc_attr( $id ) ?>">
-					<input type="checkbox" if="<?php esc_attr( $id ) ?>" name="<?php esc_attr( $name ) ?>" value="1" <?php echo ( $value == 1 ) ? 'checked="checked"' : ''; ?>>
-					<?php echo esc_attr( $label ) ?>
-				</label>
-			</p>
+		<p class="description description-wide <?php echo esc_attr( $class ) ?>">
+			<label for="<?php echo esc_attr( $id ); ?>"><input type="checkbox" id="<?php echo esc_attr( $id ); ?>" name="<?php echo esc_attr( $name ); ?>" value="1" <?php echo ( $value == 1 ) ? 'checked="checked"' : ''; ?> /><?php echo esc_attr( $label ); ?></label>
+		</p>
 		<?php
 	endforeach;
 }
-add_action( 'wp_nav_menu_item_custom_fields', 'megamenu_fields', 10, 4);
-
+add_action( 'wp_nav_menu_item_custom_fields', 'megamenu_fields', 10, 4 );
 // Create Columns
-function megamenu_columns( $columns ){
-
+function megamenu_columns( $columns ) {
 	$fields = fields_list();
 	$columns = array_merge( $columns, $fields );
-	return $columns; 
-
+	return $columns;
 }
-add_filter( 'manage_nav-menus_columns', 'megamenu_columns', 99);
-
-// Save Fields
-function megamenu_save_update( $menu_id, $menu_item_db_id, $menu_item_args ){
-
-	// NO AJAX = NO AUTOSAVE
-	if( defined( 'DOING_AJAX' ) && DOING_AJAX ) {
+add_filter( 'manage_nav-menus_columns', 'megamenu_columns', 99 );
+// Save fields
+function megamenu_save( $menu_id, $menu_item_db_id, $menu_item_args ) {
+	if ( defined( 'DOING_AJAX' ) && DOING_AJAX ) {
 		return;
 	}
-	
-	// SAVE BY CLICK SaveButton
-	check_admin_referer( 'update-nav_menu', 'update-nav-menu-nonce');
-
+	check_admin_referer( 'update-nav_menu', 'update-nav-menu-nonce' );
 	$fields = fields_list();
-
-	foreach( $fields as $_key => $label ) {
-
+	foreach ( $fields as $_key => $label ) {
 		$key = sprintf( 'menu-item-%s', $_key );
-		// Sanitize
-		if( ! empty( $_POST[ $key ][ $menu_item_db_id ] ) ){
+		// Sanitize.
+		if ( ! empty( $_POST[ $key ][ $menu_item_db_id ] ) ) {
+			// Do some checks here...
 			$value = $_POST[ $key ][ $menu_item_db_id ];
 		} else {
 			$value = null;
 		}
-		// Save or Update
-		if( ! is_null( $value ) ) {
+		// Update.
+		if ( ! is_null( $value ) ) {
 			update_post_meta( $menu_item_db_id, $key, $value );
 			echo "key:$key<br />";
 		} else {
@@ -88,16 +59,12 @@ function megamenu_save_update( $menu_id, $menu_item_db_id, $menu_item_args ){
 		}
 	}
 }
-add_action( 'wp_update_nav_menu_item', 'megamenu_save_update', 10 , 3 );
-
-// UPDATE the WALKER NAV CLASS
-function megamenu__filter_walkernav( $walker ){
-	$walker = 'Megamenu_Walker_Edit';
-	if( ! class_exists( $walker ) ){
-		require_once dirname(__FILE__) . '/templates/walker-edit.php';
-	}
-	return $walker;
+add_action( 'wp_update_nav_menu_item', 'megamenu_save', 10, 3 );
+function megamenu_filter_walker( $walker ) {
+    $walker = 'MegaMenu_Walker_Edit';
+    if ( ! class_exists( $walker ) ) {
+        require_once dirname( __FILE__ ) . '/templates/walker-edit.php';
+    }
+    return $walker;
 }
-add_filter( 'wp_edit_nav_menu_walker', 'megamenu__filter_walkernav', 99);
-
-
+add_filter( 'wp_edit_nav_menu_walker', 'megamenu_filter_walker', 99 );
